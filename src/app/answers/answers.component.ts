c78c7a486b11e93622c7111c5591e71d22b4eba8
Sam Spiers <sam.spiers@libertymutual.com>
2017-12-21 22:07:15 -0500

    make a simple jeopardy game

---
+++ src/app/answers/answers.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-answers',
+  templateUrl: './answers.component.html',
+  styleUrls: ['./answers.component.css']
+})
+export class AnswersComponent implements OnInit {
+
+
+  savedAnswer: string;
+
+  @Output() buttonClicked = new EventEmitter();
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  submitAnswer() {
+    this.buttonClicked.emit(this.savedAnswer);
+  }
+
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {JeopardyService } from './jeopardy.service';
+import { Component, OnInit} from '@angular/core';
+import { JeopardyService } from './jeopardy.service';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +10,39 @@ export class AppComponent implements OnInit {
   title = 'app';
 
   questionInfo;
+  score: number = 0;
 
-  constructor(private jeopardyService: JeopardyService){}
+  
+  constructor(private jeopardyService: JeopardyService) { }
 
-  getDataFromService(){
+  getDataFromService() {
     this.jeopardyService.getQuestionInfo()
       .subscribe(
-        questionInfo => {
-          this.questionInfo = questionInfo[0];
-        }
+      questionInfo => {
+        this.questionInfo = questionInfo[0];
+      }
       )
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.getDataFromService()
   }
 
+
+  isCorrect(savedAnswer: string) {
+    if (savedAnswer.toLowerCase == this.questionInfo.answer.toLowerCase) {
+      this.score += this.questionInfo.value;
+      alert("that is correct");
+      savedAnswer = "";
+    } else {
+      this.score -= this.questionInfo.value;
+      alert("that is wrong!")
+      savedAnswer = "";
+    }
+    this.getDataFromService();
+  }
+
+
 }
+
+
